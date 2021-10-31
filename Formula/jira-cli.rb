cc407559072c28ebc181c13ba80bd56d4ccc784b
Ankit Pokhrel <ankitpokhrel@gmail.com>
2021-10-31 19:15:40 +0100

    Fix head installation and some refactoring

--- Formula/jira-cli.rb
+++ Formula/jira-cli.rb
@@ -16,20 +16,10 @@ class JiraCli < Formula
     if Hardware::CPU.intel?
       url "https://github.com/ankitpokhrel/jira-cli/releases/download/v0.1.0/jira_0.1.0_macOS_x86_64.tar.gz"
       sha256 "c8d99498392108afbe103e9d63aaf8697377743cb5f8aa81674b90c81d6c45eb"
-
-      def install
-        system "make", "install" if build.head?
-        bin.install File.exist?("bin/jira") ? "bin/jira" : "jira"
-      end
     end
     if Hardware::CPU.arm?
       url "https://github.com/ankitpokhrel/jira-cli/releases/download/v0.1.0/jira_0.1.0_macOS_arm64.tar.gz"
       sha256 "aa6b38ac7b8f5026b8de0a0a4a69ceb63561baa43d6fcfa723b7299057e12b6a"
-
-      def install
-        system "make", "install" if build.head?
-        bin.install File.exist?("bin/jira") ? "bin/jira" : "jira"
-      end
     end
   end
 
@@ -37,29 +27,23 @@ class JiraCli < Formula
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/ankitpokhrel/jira-cli/releases/download/v0.1.0/jira_0.1.0_linux_arm64.tar.gz"
       sha256 "b9bdad9bb48bc94810ac941395ddc2eb6469b8c518ed505a70e26e2aafdf231e"
-
-      def install
-        system "make", "install" if build.head?
-        bin.install File.exist?("bin/jira") ? "bin/jira" : "jira"
-      end
     end
     if Hardware::CPU.intel?
       url "https://github.com/ankitpokhrel/jira-cli/releases/download/v0.1.0/jira_0.1.0_linux_x86_64.tar.gz"
       sha256 "750c48bf6abc088378a472f60e92ddf500706ab65d88478dfe459766714d04af"
-
-      def install
-        system "make", "install" if build.head?
-        bin.install File.exist?("bin/jira") ? "bin/jira" : "jira"
-      end
     end
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
       url "https://github.com/ankitpokhrel/jira-cli/releases/download/v0.1.0/jira_0.1.0_linux_armv6.tar.gz"
       sha256 "c887a81aed3a0b8db69ec6ef56e6c7e7744e86c672df68a97fff3ff7f58a1433"
-
-      def install
-        system "make", "install" if build.head?
-        bin.install File.exist?("bin/jira") ? "bin/jira" : "jira"
-      end
+    end
+  end
+  
+  def install
+    if build.head?
+      system "make", "install"
+      bin.install ENV["GOPATH"] + "/bin/jira"
+    else
+      bin.install File.exist?("bin/jira") ? "bin/jira" : "jira"
     end
   end
 
